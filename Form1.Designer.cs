9fa354d405dbbaa52d05a70ad99fd5fee07e3923
michael.vara <michael.vara@cpnv.ch>
2018-11-27 13:49:10 +0100

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Tetris
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtPlay = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Tetris.Properties.Resources.TetrisBlocksLettres;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1035, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxtPlay
+            // 
+            this.TxtPlay.AutoSize = true;
+            this.TxtPlay.Font = new System.Drawing.Font("Lucida Calligraphy", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPlay.ForeColor = System.Drawing.Color.White;
+            this.TxtPlay.Location = new System.Drawing.Point(403, 283);
+            this.TxtPlay.Name = "TxtPlay";
+            this.TxtPlay.Size = new System.Drawing.Size(189, 68);
+            this.TxtPlay.TabIndex = 1;
+            this.TxtPlay.Text = "Jouer";
+            this.TxtPlay.Click += new System.EventHandler(this.TxtPlay_Click);
+            this.TxtPlay.MouseEnter += new System.EventHandler(this.TxtPlay_MouseEnter);
+            this.TxtPlay.MouseLeave += new System.EventHandler(this.TxtPlay_MouseLeave);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(1021, 677);
+            this.Controls.Add(this.TxtPlay);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1037, 716);
+            this.MinimumSize = new System.Drawing.Size(1037, 716);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label TxtPlay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void TxtPlay_MouseEnter(object sender, EventArgs e)
+        {
+            TxtPlay.ForeColor = Color.Silver;
+        }
+
+        private void TxtPlay_MouseLeave(object sender, EventArgs e)
+        {
+            TxtPlay.ForeColor = Color.White;
+        }
+
+        private void TxtPlay_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
