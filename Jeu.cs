3ea59d47db80bf9edca3b2d1051de43dff20559b
michael.vara <michael.vara@cpnv.ch>
2018-11-29 16:29:48 +0100

    Next Forme
    Ajout d'une fonction pour choisir et afficher aléatoirement la prochaine forme

--- Jeu.cs
+++ Jeu.cs
@@ -13,12 +13,25 @@ namespace Tetris
     public partial class frmJeu : Form
     {
         #region private attributes
+        private enum _forme
+        {
+            baton,
+            cube,
+            l1,
+            l2,
+            s,
+            t,
+            z
+        }
+        private string _nextForme = null;
         #endregion private attributes
 
         #region constructors
         public frmJeu()
         {
             InitializeComponent();
+
+            _nextForme = NextForme();
         }
         #endregion constructors
 
@@ -29,6 +42,43 @@ namespace Tetris
         #endregion public methods
 
         #region private methods
+        private string NextForme()
+        {
+            _forme f = (_forme)(new Random()).Next(0, 7);
+
+            switch(f)
+            {
+                case _forme.baton:
+                    this.ptbNextBaton.Show();
+                    break;
+
+                case _forme.cube:
+                    this.ptbNextCube.Show();
+                    break;
+
+                case _forme.l1:
+                    this.ptbNextL1.Show();
+                    break;
+
+                case _forme.l2:
+                    this.ptbNextL2.Show();
+                    break;
+
+                case _forme.s:
+                    this.ptbNextS.Show();
+                    break;
+
+                case _forme.t:
+                    this.ptbNextT.Show();
+                    break;
+
+                case _forme.z:
+                    this.ptbNextZ.Show();
+                    break;
+            }
+
+            return f.ToString();
+        }
         #endregion private methods
     }
 }
